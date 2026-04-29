a14776c2018bee5a60940d5f32feb0f0e220b2bb
Ubuntu <ubuntu@fv46sn1mhqgof1ot08jt.auto.internal>
2026-04-29 18:11:03 +0000

    Fix cat image serialization

--- backend/cats/serializers.py
+++ backend/cats/serializers.py
@@ -1,19 +1,24 @@
 import base64
 import datetime as dt
+
 import webcolors
 from django.core.files.base import ContentFile
 from rest_framework import serializers
+
 from .models import Achievement, AchievementCat, Cat
+
 
 class Hex2NameColor(serializers.Field):
     def to_representation(self, value):
         return value
+
     def to_internal_value(self, data):
         try:
             data = webcolors.hex_to_name(data)
         except ValueError:
             raise serializers.ValidationError('Для этого цвета нет имени')
         return data
+
 
 class AchievementSerializer(serializers.ModelSerializer):
     achievement_name = serializers.CharField(source='name')
@@ -22,70 +27,82 @@ class AchievementSerializer(serializers.ModelSerializer):
         model = Achievement
         fields = ('id', 'achievement_name')
 
+
 class Base64ImageField(serializers.ImageField):
     def to_internal_value(self, data):
         if isinstance(data, str) and data.startswith('data:image'):
-            format, imgstr = data.split(';base64,')
-            ext = format.split('/')[-1]
-            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
+            format_data, imgstr = data.split(';base64,')
+            ext = format_data.split('/')[-1]
+            data = ContentFile(base64.b64decode(imgstr), name=f'temp.{ext}')
         return super().to_internal_value(data)
+
 
 class CatSerializer(serializers.ModelSerializer):
     achievements = AchievementSerializer(required=False, many=True)
     color = Hex2NameColor()
     age = serializers.SerializerMethodField()
-    image = Base64ImageField(required=False, allow_null=True)
+    image = serializers.SerializerMethodField()
+    uploaded_image = Base64ImageField(write_only=True, required=False, allow_null=True)
 
     class Meta:
         model = Cat
         fields = (
-            'id', 'name', 'color', 'birth_year', 'achievements', 'owner', 'age',
-            'image'
-            )
+            'id',
+            'name',
+            'color',
+            'birth_year',
+            'achievements',
+            'owner',
+            'age',
+            'image',
+            'uploaded_image',
+        )
         read_only_fields = ('owner',)
 
     def get_age(self, obj):
         return dt.datetime.now().year - obj.birth_year
 
     def get_image(self, obj):
-        # Это гарантирует, что мы отдаем URL, 
-        # который будет резолвиться правильно относительно хоста
         if obj.image:
             return obj.image.url
         return None
 
     def create(self, validated_data):
-        if 'achievements' not in self.initial_data:
-            cat = Cat.objects.create(**validated_data)
-            return cat
-        else:
-            achievements = validated_data.pop('achievements')
-            cat = Cat.objects.create(**validated_data)
-            for achievement in achievements:
-                current_achievement, status = Achievement.objects.get_or_create(
-                    **achievement
-                    )
-                AchievementCat.objects.create(
-                    achievement=current_achievement, cat=cat
-                    )
-            return cat
+        uploaded_image = validated_data.pop('uploaded_image', None)
+        achievements = validated_data.pop('achievements', [])
+
+        cat = Cat.objects.create(**validated_data)
+
+        if uploaded_image is not None:
+            cat.image = uploaded_image
+            cat.save()
+
+        for achievement in achievements:
+            current_achievement, _ = Achievement.objects.get_or_create(**achievement)
+            AchievementCat.objects.create(
+                achievement=current_achievement,
+                cat=cat
+            )
+
+        return cat
 
     def update(self, instance, validated_data):
+        uploaded_image = validated_data.pop('uploaded_image', None)
+        achievements_data = validated_data.pop('achievements', None)
+
         instance.name = validated_data.get('name', instance.name)
         instance.color = validated_data.get('color', instance.color)
-        instance.birth_year = validated_data.get(
-            'birth_year', instance.birth_year
-            )
-        instance.image = validated_data.get('image', instance.image)
-        if 'achievements' in validated_data:
-            achievements_data = validated_data.pop('achievements')
-            lst = []
+        instance.birth_year = validated_data.get('birth_year', instance.birth_year)
+
+        if uploaded_image is not None:
+            instance.image = uploaded_image
+
+        if achievements_data is not None:
+            achievements_list = []
             for achievement in achievements_data:
-                current_achievement, status = Achievement.objects.get_or_create(
-                    **achievement
-                    )
-                lst.append(current_achievement)
-            instance.achievements.set(lst)
+                current_achievement, _ = Achievement.objects.get_or_create(**achievement)
+                achievements_list.append(current_achievement)
+            instance.achievements.set(achievements_list)
 
         instance.save()
         return instance
